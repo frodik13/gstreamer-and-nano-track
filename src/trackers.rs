29e8b6a3185763e94c291f280fc5e463fd3d823b
Fedor <frodo-66@yandex.ru>
2025-09-18 11:23:16 +0400

    add nano track v3 model

--- src/trackers.rs
+++ src/trackers.rs
@@ -14,10 +14,10 @@ impl NanoTrack {
     {
         let head = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("models")
-            .join("nanotrack_head_sim.onnx");
+            .join("nanotrack_head.onnx");
         let backbone = Path::new(env!("CARGO_MANIFEST_DIR"))
             .join("models")
-            .join("nanotrack_backbone_sim.onnx");
+            .join("nanotrack_backbone.onnx");
 
         let mut param = TrackerNano_Params::default()?;
         param.set_backbone(backbone.to_str().unwrap());
