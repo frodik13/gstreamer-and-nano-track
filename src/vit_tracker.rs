9602be7915ee4eddcdf4a41f733c188faa4d77d5
Fedor <frodo-66@yandex.ru>
2025-09-19 14:37:49 +0400

    add vit with second vit trackers

--- src/vit_tracker.rs
+++ src/vit_tracker.rs
@@ -5,6 +5,9 @@ use std::path::Path;
 
 pub struct VitTracker {
     tracker: Ptr<TrackerVit>,
+    second_tracker: Ptr<TrackerVit>,
+    last_bbox: Option<Rect>,
+    last_frame: Option<Mat>,
 }
 
 impl VitTracker {
@@ -20,7 +23,23 @@ impl VitTracker {
         let mut tracker = TrackerVit::create(&param)?;
         tracker.init(frame, initial_bbox)?;
 
-        Ok(VitTracker { tracker })
+        let model_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("models")
+            .join("object_tracking_vittrack_2023sep.onnx");
+
+        let mut param = TrackerVit_Params::default()?;
+        param.set_backend(opencv::dnn::DNN_BACKEND_OPENCV);
+        param.set_target(opencv::dnn::DNN_TARGET_CPU);
+        param.set_net(model_path.to_str().unwrap());
+
+        let second_tracker = TrackerVit::create(&param)?;
+
+        Ok(VitTracker {
+            tracker,
+            second_tracker,
+            last_bbox: Some(initial_bbox),
+            last_frame: Some(frame.input_array()?.get_mat(0)?),
+        })
     }
 
     pub fn update(&mut self, frame: &Mat) -> opencv::Result<Option<Rect>> {
@@ -31,9 +50,39 @@ impl VitTracker {
         println!("Score vit tracker: {:?}", score);
 
         if score >= 0.45 {
+            self.last_bbox = Some(bbox);
+            self.last_frame = Some(frame.clone());
             Ok(Some(bbox))
         } else {
-            Ok(None)
+            println!("initial second tracker");
+
+            if let Some(last_bbox) = self.last_bbox {
+                if let Some(last_frame) = &self.last_frame {
+                    self.second_tracker.init(last_frame, last_bbox)?;
+                    self.second_tracker.update(frame, &mut bbox)?;
+
+                    let score = self.second_tracker.get_tracking_score()?;
+                    println!("\tScore second tracker: {:?}", score);
+
+                    if score >= 0.55 {
+                        self.last_bbox = Some(bbox);
+                        self.last_frame = Some(frame.clone());
+                        Ok(Some(bbox))
+                    } else {
+                        self.return_none()
+                    }
+                } else {
+                    self.return_none()
+                }
+            } else {
+                self.return_none()
+            }
         }
+    }
+
+    fn return_none(&mut self) -> opencv::Result<Option<Rect>> {
+        self.last_bbox = None;
+        self.last_frame = None;
+        Ok(None)
     }
 }
